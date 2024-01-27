881f50bc91e8ba050fb7c14d7a6d721d8adaa7eb
ShyamPandit3107 <shyambpandit3107@gmail.com>
2024-01-27 16:20:25 +0530

    added seeders and factory in the code

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,15 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(3),
+            'body' => [],
+
         ];
+    }
+    public function untitled()
+    {
+        return $this->state([
+            'title' => 'untitled'
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,8 +6,9 @@ use Database\Seeders\Traits\DisableForeignKeys;
 use Database\Seeders\Traits\TruncateTable;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class PostSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     use TruncateTable, DisableForeignKeys;
     /**
@@ -16,8 +17,9 @@ class PostSeeder extends Seeder
     public function run(): void
     {
         $this->disableForeignKeys();
-        $this->truncate('posts');
-        \App\Models\Post::factory(3)->untitled()->create();
+        $this->truncate('users');
+        \App\Models\User::factory(10)->create();
         $this->enableForeignKeys();
+
     }
 }
